18e38ec4a918a6a00c68ee769e94e723b5347fa4
Heming <git@heming.dev>
2025-08-11 10:46:27 +0200

    bundle gem pogospec-rails

---
+++ lib/pogocache/rails.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rails/version"
+
+module Pogocache
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/pogocache/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pogocache::Rails do
+  it "has a version number" do
+    expect(Pogocache::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
